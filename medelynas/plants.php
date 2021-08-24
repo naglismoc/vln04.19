f837ca452b1ceffa7a7b7ac17d14ae5db441c8fe
naglis mockevicius <48673743+naglismoc@users.noreply.github.com>
2021-08-24 11:26:05 +0300

    one to many

--- medelynas/plants.php
+++ medelynas/plants.php
@@ -4,25 +4,25 @@
 
 
 
-include('./DB.php');
+include('./uniquePlants.php');
 
 
 
     if(isset($_POST['create'] )){
         store();
-        header("location:./");
+        header("location:./plants.php?id=".$_POST['plant_id']);
         die;
     }
 
     if(isset($_POST['update'] )){
         update();
-        header("location:./");
+        header("location:./plants.php?id=".$_POST['plant_id']);
         die;
     }
     
     if(isset($_POST['delete'] )){
         destroy($_POST['delete']);
-        header("location:./");
+        header("location:./plants.php?id=".$_POST['plant_id']);
         die; 
     }
 
@@ -67,28 +67,29 @@ include('./DB.php');
     </style>
 </head>
 <body>
-
+<div class="container">
+    <a href="./">HOME</a>
     <form class="form" action="" method="POST">
       
         <div class="form-group row">
-            <label class="col-sm-2 col-form-label" >Augalo pavadinimas</label>
+            <label class="col-sm-2 col-form-label" >age</label>
             <div class="col-sm-4">
-                <input class="form-control" type="text" name="name" value="<?= (isset($plant))? $plant['name'] : "" ?>">
+                <input class="form-control" type="text" name="age" value="<?= (isset($plant))? $plant['age'] : "" ?>">
             </div>
          </div>
          <div class="form-group row">
-            <label class="col-sm-2 col-form-label" >Vienmetis</label>
+            <label class="col-sm-2 col-form-label" >height</label>
             <div class="col-sm-4">
-                <input type="checkbox" name="is_yearling">
-                <!-- <input class="form-control" type="text" name="is_yearling" value="<?= (isset($plant))? $plant['is_yearling'] : "" ?>"> -->
+                <input class="form-control" type="text" name="height" value="<?= (isset($plant))? $plant['height'] : "" ?>">
             </div>
          </div>
          <div class="form-group row">
-            <label class="col-sm-2 col-form-label" >Kiekis</label>
+            <label class="col-sm-2 col-form-label" >health</label>
             <div class="col-sm-4">
-                <input class="form-control" type="text" name="quantity" value="<?= (isset($plant))? $plant['quantity'] : "" ?>">
+                <input class="form-control" type="text" name="health" value="<?= (isset($plant))? $plant['health'] : "" ?>">
             </div>
          </div>
+         <input type="hidden" name="plant_id" value="<?=$_GET['id']?>">
     <?php if(!isset($plant)){
             echo '<button class="btn btn-primary" name="create" type="submit">Pridėti augalą</button>';
     }else{
@@ -101,41 +102,46 @@ include('./DB.php');
     <table class="table">
         <tr>
         <th>Id</th> 
-        <th>Rūšis</th> 
-        <th>vienmetis</th> 
-        <th>kiekis</th> 
-        <th>augalai</th> 
+        <?php
+        $all = all();
+            if( isset($all->fetch_assoc()['name'])){
+                echo "  <th>name</th>  ";
+            }
+        ?>
+        <th>age</th> 
+        <th>height</th> 
+        <th>health</th> 
         <th>edit</th> 
         <th>delete</th> 
         </tr>
 
 
-        <?php $count = 0; foreach (allOld() as $plant) { 
-            $checked = "";
-            if( $plant['is_yearling'] ){
-                $checked = "checked";
-            }
-            
+        <?php $count = 0; foreach ($all as $plant) {
+           
             ?>
             <tr>
-            <td> <?= ++$count."/".$plant['id']  ?> </td>
-                <td> <?= $plant['name']  ?> </td>
-                <td> 
-                    <input type="checkbox" name="" id="" <?=$checked?> disabled>
-                </td>
-                <td> <?= $plant['quantity']  ?> </td>
-                <td>     <a class="btn btn-primary" href="./plants.php?id=<?= $plant['id']  ?>">augalai</a> </td>
+                <td> <?= ++$count."/".$plant['id']  ?> </td>
+                <?php
+                    if( isset($plant['name'])){
+                        echo "  <td>".$plant['name']."</td>  ";
+                    }
+                ?>
+                <td> <?= $plant['age']  ?> </td>
+                <td> <?= $plant['height']  ?> </td>
+                <td> <?= $plant['health']  ?> </td>
                 <td>
                  
-                    <a class="btn btn-success" href="?edit=<?= $plant['id']  ?>">edit</a>
+                    <a class="btn btn-success" href="?id=<?=    $_GET['id']?>&edit=<?= $plant['id']  ?>">edit</a>
                 </td>
                 <td>
                     <form action="" method="post">
+                        <input type="hidden" name="plant_id" value="<?=$_GET['id']?>">
                         <button class="btn btn-danger" type="submit" name="delete" value="<?=$plant['id']?>"  >delete</button>
                     </form>
                 </td>
             </tr>
         <?php } ?>
     </table>
+    </div>
 </body>
 </html>
---
+++ medelynas/uniquePlants.php
@@ -0,0 +1,60 @@
+<?php
+
+function connect()
+{
+    return new mysqli("localhost","root","","medelynas");
+}
+
+function find($id){
+    $conn = connect();
+    $sql = 'SELECT * from `unique_plants` where id ='.$id;
+    $result = $conn->query($sql);
+    $conn->close();
+    return $result->fetch_assoc();
+}
+
+
+function all(){
+    $conn = connect();
+    $sql = "SELECT `unique_plants`.`id`, `name`, `age`, `height`, `health` 
+            FROM `unique_plants`
+            INNER join `plants`
+            ON `plants`.`id` = `unique_plants`.`plant_id`";
+    if(isset($_GET['id'])){
+        $sql = "SELECT * from `unique_plants` where plant_id = ".$_GET['id'];
+    }
+    $result = $conn->query($sql);
+    $conn->close();
+    return $result;
+}
+
+function store(){
+    $conn = connect();
+
+    $sql = "INSERT INTO `unique_plants` (`id`, `age`, `height`, `health`, `plant_id`) 
+            VALUES (NULL, '".$_POST['age']."',  '".$_POST['height']."',  '".$_POST['health']."', '".$_POST['plant_id']."');";
+  
+//   echo $sql;die;
+    $conn->query($sql);
+    $conn->close();
+}
+
+
+function update(){
+    $conn = connect(); $sql = "UPDATE `unique_plants`
+        SET `age` = '".$_POST['age']."', `height` = '".$_POST['height']."', `health` = '".$_POST['health']."'
+        WHERE `unique_plants`.`id` = '".$_POST['update']."';";
+   
+    $conn->query($sql);
+    $conn->close();
+}
+
+function destroy($id){
+    $conn = connect();
+    $sql = "DELETE FROM `unique_plants` WHERE id=".$id;
+    $conn->query($sql);
+    $conn->close();
+}
+
+
+?>
